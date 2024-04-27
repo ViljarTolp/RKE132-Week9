14071004905b3e0fc29506dfec298bc8abe14e77
ViljarT <viljar.tolp@tktk.ee>
2024-04-27 17:35:32 +0300

    Valma

---
+++ Test/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+static void DisplayDataFromFile(List<string> someList)
+{
+    foreach (string line in someList)
+    {
+        Console.WriteLine(line);
+    }
+}
